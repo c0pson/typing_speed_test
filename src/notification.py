a50d0db2fb3bfff47ffd3e0528703b3e4caa0801
c0pson <pc312199@student.polsl.pl>
2024-08-06 11:26:21 +0200

    fixed visibility

--- src/notification.py
+++ src/notification.py
@@ -8,12 +8,12 @@ class Notification(ctk.CTkLabel):
     def __init__(self, master, text: str, duration: float):
         super().__init__(master, fg_color=COLOR.TRANSPARENT,
                         text=text, text_color=COLOR.BAD,
-                        font=ctk.CTkFont('Source Code Pro', 14))
+                        font=ctk.CTkFont('Source Code Pro', 24))
         self.duration = duration
         self.duration_ = duration
-        self.random_position = uniform(0.3, 0.6)
+        self.random_position = uniform(0.2, 0.8)
         self.random_end_pos = uniform(0.1, 0.9)
-        self.place(x=-10, y=-10)
+        self.place(x=-100, y=-100)
         pywinstyles.set_opacity(self, value=0.5, color='#000001')
         self.master.after(21, lambda: self.place_animate(0))
 
