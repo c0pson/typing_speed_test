dac945b78860f0876e0a176c40c3c488446e62c4
c0pson <pc312199@student.polsl.pl>
2024-08-06 11:58:33 +0200

    colors

--- src/properties.py
+++ src/properties.py
@@ -3,7 +3,9 @@ from enum import Enum
 class COLOR(str, Enum):
     TRANSPARENT = 'transparent'
     BACKGROUND = '#352F44'
+    ACCURACY = '#B4D6CD'
     TEXT = '#FFFAE6'
     GOOD = '#A2CA71'
     BAD =  '#F5004F'
     GRAY = '#686D76'
+    WPM = '#C63C51'
