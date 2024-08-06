9ffa57394863aeb63f757eda8f594ca0c70086ba
c0pson <pc312199@student.polsl.pl>
2024-08-06 11:26:34 +0200

    main

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import customtkinter as ctk
 from typing_test import TypingTest
 from tools import resource_path
 from properties import COLOR
+import user_statistics
 from menu import Menu
 
 class MainWindow(ctk.CTk):
@@ -12,6 +13,7 @@ class MainWindow(ctk.CTk):
         self.load_font()
         self.title('Type speed')
         self.iconbitmap(resource_path('assets\\icon.ico'))
+        user_statistics.setup_database()
         self.type_test = TypingTest(self)
         self.menu = Menu(self, self.type_test)
         self.type_test.pack(padx=20, pady=20, expand=True)
@@ -25,6 +27,12 @@ class MainWindow(ctk.CTk):
 
     def update_counter(self, wpm: float, cpm: float) -> None:
         self.menu.update_wpm_cpm(wpm, cpm)
+
+    def update_percentage(self, accuracy: int) -> None:
+        self.menu.update_accuracy(accuracy)
+
+    def insert_to_db(self, wpm: float, cpm: float, accuracy: int) -> None:
+        user_statistics.insert_stat(wpm, cpm, accuracy)
 
     def toggle_fullscreen(self, event=None):
         self.attributes('-fullscreen', True)
